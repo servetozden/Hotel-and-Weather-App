edad24baddfddb8f07b4b76c981e3b9239c2ebd7
Servet Serhat Özden <servetozden50@hotmail.com>
2022-01-09 01:40:27 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/foursquareproject/MainActivity.java
+++ app/src/main/java/com/example/foursquareproject/MainActivity.java
@@ -6,14 +6,18 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.foursquareproject.databinding.ActivityMainBinding;
 import com.example.foursquareproject.fragments.ChooseCityFragment;
 import com.example.foursquareproject.listeners.MainPageListeners;
+import com.example.foursquareproject.viewmodel.MainViewModel;
 import com.google.gson.Gson;
 
 import org.jetbrains.annotations.NotNull;
@@ -30,24 +34,41 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements MainPageListeners {
 
-
-    private ActivityMainBinding mainBinding;
+    private MainViewModel viewModel;
+    public ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
 
 
 
      binding.searchButton.setOnClickListener(view -> {
-         //acıalcak bu comment
-         /*binding.constraintScreen.setVisibility(View.GONE);
-         getSupportFragmentManager().beginTransaction().replace(R.id.containerFragment,new ChooseCityFragment()).commit();*/
 
-         Intent myIntent = new Intent(MainActivity.this, WeatherActivity.class);
+         String nameSize = String.valueOf(binding.textUserName.getText());
+         if (nameSize == null){
+             Toast.makeText(this,"Lütfen isim giriniz!", Toast.LENGTH_LONG);
+         }else{
+             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+             SharedPreferences.Editor editor = preferences.edit();
+             editor.putString("person", String.valueOf(binding.textUserName.getText()));
+             editor.apply();
 
-         MainActivity.this.startActivity(myIntent);
+             binding.constraintScreen.setVisibility(View.GONE);
+             getSupportFragmentManager().beginTransaction().replace(R.id.containerFragment,new ChooseCityFragment()).commit();
+
+         }
+
+              /* Intent myIntent = new Intent(MainActivity.this, WeatherActivity.class);
+         MainActivity.this.startActivity(myIntent);*/
+
+     });
+
+     binding.textSelectCity.setOnClickListener(view -> {
+        // viewModel.getCityPosition();
+
 
      });
     }
@@ -55,8 +76,15 @@ public class MainActivity extends AppCompatActivity implements MainPageListeners
 
     @Override
     public void onClickItem(String items) {
-        Toast.makeText(this, "ssss",
-                Toast.LENGTH_SHORT).show();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("city",items);
+        editor.apply();
+        Intent intent = new Intent(this, WeatherActivity.class);
+        startActivity(intent);
+
+
+
     }
 
 
